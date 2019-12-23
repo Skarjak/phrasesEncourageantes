5fd7c9da456fda06236846eecdb39a8fba3f5b28
skarjak <er.blais@gmail.com>
2019-12-23 01:18:00 -0500

    Finally got music pause/toggle to work.

--- app/src/main/java/com/example/encourage/MainActivity.java
+++ app/src/main/java/com/example/encourage/MainActivity.java
@@ -8,22 +8,31 @@ import android.animation.ObjectAnimator;
 import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.media.MediaPlayer.SEEK_CLOSEST;
 import static java.lang.Math.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private TextView musicCredit;
     private Button musicToggle;
     private String[] sentences;
     private ObjectAnimator fadeIn;
     private ObjectAnimator fadeOut;
+    private ObjectAnimator creditFade;
     private int fadeInTime;
     private int fadeOutTime;
     private MediaPlayer mediaPlayer;
+    private int musicPosition = 0;
+    private static final String TAG = "Main activity";
+    static final String STATE_MUSIC = "musicPosition";
+    static final String STATE_SILENCED = "musicSilenced";
+    boolean silenced = false;
 
 
     @Override
@@ -33,14 +42,67 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
         sentences = res.getStringArray(R.array.sentences);
         textView = findViewById(R.id.textView);
+        musicCredit = findViewById(R.id.musicCredit);
         musicToggle = findViewById(R.id.musicToggle);
         textView.setText(res.getString(R.string.first));
         fadeInTime = 800;
         fadeOutTime = 600;
-        //pas oublier d'attribuer la musique a Podington Bear -Starling, soundofpicture.com
+        if(savedInstanceState != null) {
+            musicPosition = savedInstanceState.getInt(STATE_MUSIC);
+            silenced = savedInstanceState.getBoolean(STATE_SILENCED);
+        }
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.starling);
-        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+        fadeOut = ObjectAnimator.ofFloat(musicCredit,"alpha",0f);
+        fadeOut.setDuration(4000);
+        fadeOut.start();
+        //next line only supported in api version 26
+        mediaPlayer.seekTo(musicPosition, SEEK_CLOSEST);
+        if (!silenced) {
+            mediaPlayer.start();
+        }
+        Log.d(TAG, "ON CREATE");
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt(STATE_MUSIC, musicPosition);
+        savedInstanceState.putBoolean(STATE_SILENCED, silenced);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //mediaPlayer.pause();
+        //musicPosition = mediaPlayer.getCurrentPosition();
+        musicPosition = mediaPlayer.getCurrentPosition();
+        mediaPlayer.pause();
+        //mediaPlayer.release();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mediaPlayer.seekTo(musicPosition, SEEK_CLOSEST);
+        if (!silenced) {
+            mediaPlayer.start();
+        }
+    }
+
+  //  @Override
+  //  protected void onStop(){
+  //      super.onStop();
+  //      mediaPlayer.stop();
+  //      mediaPlayer.release();
+  //  }
 
     //called when screen is tapped
     public void tapSentence(View view) {
@@ -63,9 +125,15 @@ public class MainActivity extends AppCompatActivity {
         fadeOut.start();
     }
 
-    //test
+    //toggles background music
     public void tapMusic(View view){
-        musicToggle.setText("lawl");
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            silenced = true;
+        }else{
+            mediaPlayer.start();
+            silenced = false;
+        }
     }
 }
 
